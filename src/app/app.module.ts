04b3d3f6b8517581c2606e350e8a9475cf1b07e0
Kramer <zack.kramer@accenture.com>
2021-09-20 10:44:09 -0400

    first component - nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponentComponent } from './nav-component/nav-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponentComponent
   ],
   imports: [
     BrowserModule
